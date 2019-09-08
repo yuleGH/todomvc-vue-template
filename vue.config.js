d97f339344182b10612ac7d41b5fd7cc1dc5e082
yule.mail@qq.com <yule.mail@qq.com>
2019-09-08 10:52:56 +0800

    init project

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+module.exports = {
+    // 选项...
+    devServer: {
+        //设置主机地址
+        host: 'localhost',
+        //设置默认端口
+        port: '8080',
+        //自动打开浏览器 open: true
+        open: 'Google Chrome'
+    }
+}
